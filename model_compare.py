fa03c50c442a738884cbc175dc237c44ee061c8d
Bemuseed <calebmullan@hotmail.com>
2023-04-22 10:51:14 +0100

    Added rankings

--- model_compare.py
+++ model_compare.py
@@ -6,7 +6,13 @@ import tensor_manager
 import merge
 import torch
 
-PATHS=["vincent_di_fate-000017.ckpt", "david_mack-000016.ckpt", "alan_lee-000017.ckpt", "ed_mell-000017.ckpt", "michael_garmash-000017.ckpt"]
+PATHS=["vincent_di_fate.ckpt", "david_mack.ckpt", "alan_lee.ckpt", "ed_mell.ckpt", "michael_garmash.ckpt"]
+
+def remove_filetype(filename):
+    for i in range(len(filename)):
+        if filename[i] == ".":
+            break
+    return filename[:i]
 
 # Calculates the degree of difference between two _flattened_ models
 def compare(model_a_weights, model_b_weights):
@@ -34,9 +40,10 @@ def process(model_paths):
         ind_a, ind_b = model_combos[i]
         model_a, model_b = model_dicts[ind_a], model_dicts[ind_b]
 
-        name_a, name_b = model_paths[ind_a], model_paths[ind_b]
-        merged_model_names += name_a + name_b + "0.5"
-        
+        name_a, name_b = remove_filetype(model_paths[ind_a]), remove_filetype(model_paths[ind_b])
+        merged_model_names.append("(" + name_a + ")" + "-" + "(" + name_b + ")" + "_0.5")
+
+
         merged_model = merge.merge(model_a, model_b, names=name_a+" and "+name_b)
         merged_models.append(merged_model)
         merged_model_tensors, _ = tensor_manager.get_tensors(merged_model)
@@ -50,7 +57,9 @@ def process(model_paths):
         av_difference = numpy.average(differences)
         combo_difference_scores.append(av_difference)
 
-    sorted_models_and_scores = sorted(zip(combo_difference_scores, merged_models, merged_model_names))
+    sorted_models_and_scores = sorted(zip(combo_difference_scores, merged_models, merged_model_names), reverse=True)
+    for i in sorted_models_and_scores:
+        print(i[2])
     combo_difference_scores = [i[0] for i in  sorted_models_and_scores]
     merged_models = [i[1] for i in  sorted_models_and_scores]
     merged_model_names = [i[2] for i in  sorted_models_and_scores]
@@ -58,3 +67,9 @@ def process(model_paths):
     print("Difference score results:")
     for i in range(len(merged_models)):
         print("[" + str(i+1) + "]: " + merged_model_names[i] + " - " + str(combo_difference_scores[i].item()))
+    
+    for i in tqdm(range(3), "Saving three most different models to disk"):
+        torch.save(merged_models[i], open(merged_model_names[i], "wb"))
+
+if __name__ == "__main__":
+    process(PATHS)
